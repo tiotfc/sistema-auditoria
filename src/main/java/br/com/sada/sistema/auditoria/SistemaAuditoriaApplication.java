92aeff381f029da380dd7cc50594d18874438b23
Henrique <henrique.tio@gmail.com>
2021-10-09 14:38:54 -0300

    first commit

---
+++ src/main/java/br/com/sada/sistema/auditoria/SistemaAuditoriaApplication.java
@@ -0,0 +1,13 @@
+package br.com.sada.sistema.auditoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAuditoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAuditoriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sada/sistema/auditoria/SistemaAuditoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sada.sistema.auditoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAuditoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
